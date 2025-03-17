0e058ebdde1ea9a6d1cfa5378181aa1dbf7eddc2
galebooster <gale-booster-4k@icloud.com>
2025-03-17 13:43:47 -0600

    Update app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaDeAmigos = [];
+const ulListaDeAmigos = document.getElementById("listaAmigos");
+
+function agregarAmigo(){
+  listaDeAmigos.push(inputAmigo.value);
+}
